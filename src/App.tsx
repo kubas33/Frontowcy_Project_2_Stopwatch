2b5bfc1924d83e1a3c96d7d82d35ed6de810de6e
kubas33 <44829224+kubas33@users.noreply.github.com>
2024-11-18 10:31:30 +0100

    Crate react template and add react-bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Container} from "react-bootstrap";
+
+function App() {
+
+  return (
+    <>
+        <Container fluid={"md"}>
+        </Container>
+    </>
+  )
+}
+
+export default App
