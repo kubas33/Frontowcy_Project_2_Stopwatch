88761082d9006f1e0c924b8142e35490a1f7af1b
kubas33 <44829224+kubas33@users.noreply.github.com>
2024-11-19 18:56:30 +0100

    removed aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
-      '@components': '/src/components',
-      '@types': '/src/types',
-    }
-  }
+    plugins: [react()],
 })
